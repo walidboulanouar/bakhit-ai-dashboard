319869b3abaa005cde38fa70c277153246985fb2
walidkohort <walid.boulanouar@kohort.eu>
2024-09-14 21:53:05 +0200

    update spinner

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -2,6 +2,6 @@
 
 export function Spinner() {
   return (
-    <div className="h-8 w-8 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div>
+    <div className="h-8 w-8 animate-spin rounded-full border-b-2 border-t-2 border-gray-900 dark:border-gray-100"></div>
   );
 }
