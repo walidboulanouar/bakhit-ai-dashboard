c8ddb99f64d0c5a79d950ff69971f1a820ea7743
walidkohort <walid.boulanouar@kohort.eu>
2024-09-14 21:54:16 +0200

    make spinner bigger

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -2,6 +2,6 @@
 
 export function Spinner() {
   return (
-    <div className="h-8 w-8 animate-spin rounded-full border-b-2 border-t-2 border-gray-900 dark:border-gray-100"></div>
+    <div className="h-28 w-28 animate-spin rounded-full border-b-2 border-t-2 border-gray-900 dark:border-gray-100"></div>
   );
 }
